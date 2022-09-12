389577485637a9a60e2619a2c50a03532b6899a6
jarrassoul <jarrassoul@students.jkuat.ac.ke>
2022-09-12 14:35:32 -0700

    Lower case Alphabets

--- 0x01-variables_if_else_while/2-print_alphabets.c
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
-
-/**
- * main - Prints the alphabet in lowercase, and then in uppercase.
- *
- * Return: Always 0.
+/*                                                                                                                                       * File: 0-positive_or_negative.c  
+ * Auth: jar Rassoul
  */
+#include <stdlib.h> 
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase, and then in uppercase.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 	char l;
-for (l = 'a'; l <= 'z'; l++)
-	putchar(l);
+ 	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
 
 putchar('\n');
 return (0);
